591607839fc7463c57bb48d72e008a441ae5d794
SurendraJaiswar <147294538+SurendraJaiswar@users.noreply.github.com>
2023-12-21 19:53:45 +0530

    Add files via upload

---
+++ Javascript Oper Part2.js
@@ -0,0 +1,28 @@
+//Problem 1
+const Number = 10;
+const result = number % 2 == 0 ? "Even" : "Odd"
+console.log(result)
+
+//Problem 2
+const a = 10, b = 20, c = 30;
+console.log(a,b,c)
+
+//problem 3
+const number = 0
+number > 0 
+? console.log("positive")
+:number < 0
+? console.log("Negative")
+:console.log("zero");
+
+//output
+//zero
+
+//Problem 4
+const age = 18
+age < 18 ? console.log("you cannot vote") : console.log("you can vote")
+
+//output
+//You can vote
+
+
